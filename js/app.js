d995411b71caae54a94ae3f2a5c49242db5632b7
emalderete <nicolas.alderete95@live.com>
2023-03-02 00:57:54 -0300

    Se consume la API
    • Se consume la API y se vinculan los datos con los de la página.

--- js/app.js
+++ js/app.js
@@ -9,6 +9,13 @@ let linkedinDiv = document.querySelector('.linkedinDiv');
 let githubDiv = document.querySelector('.githubDiv');
 let twitterDiv = document.querySelector('.twitterDiv');
 let social = document.querySelector('.social');
+let fullName = document.querySelector('#fullName');
+let age = document.querySelector('#age');
+let country = document.querySelector('#country');
+let mail = document.querySelector('#mail');
+let address = document.querySelector('#address');
+let image = document.querySelector('#profileImagePhoto');
+var randomUser;
 
 twitterLink.addEventListener('mouseover', () => { cssSocialCorrectionIn(twitterDiv) });
 twitterLink.addEventListener('mouseout', () => { cssSocialCorrectionOut(twitterDiv) });
@@ -34,3 +41,25 @@ function cssSocialCorrectionOut(element) {
     element.classList.toggle('d-none');
     social.style.marginTop = '2.5rem';
 }
+
+
+async function fetchData() {
+    let response = await fetch('https://randomuser.me/api/');
+    let dataJson = response.json();
+    return dataJson;
+}
+
+async function dataModifier() {
+    let data = await fetchData();
+    let dataString = JSON.stringify(data);
+    let dataParse = JSON.parse(dataString);
+    randomUser = dataParse.results;
+    fullName.innerHTML = `${randomUser[0].name.first} ${randomUser[0].name.last}`;
+    image.setAttribute('src', `${randomUser[0].picture.large}`);
+    age.innerHTML = `${randomUser[0].dob.age}`;
+    country.innerHTML = `${randomUser[0].location.country}`;
+    mail.innerHTML = `${randomUser[0].email}`;
+    address.innerHTML = `${randomUser[0].location.city}`;
+}
+
+dataModifier();
